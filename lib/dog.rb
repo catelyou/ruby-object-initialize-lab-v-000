497a8ae3eebb435ad59b26a3bb6c7b3dc80fcc1c
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-13 16:33:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,5 @@
 class Dog
 
-  def initialize(breed)
-    @breed = breed
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-
   def initialize(name)
     @name = name
   end
